8dc706b0306d3c78ee2ca2ff3a6b109fa7b34f87
Ольга Егорова <hardcorecase@gmail.com>
2023-01-20 15:56:46 +0300

    Initial Commit

---
+++ CoinsCoursework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinsCoursework
+//
+//  Created by Ольга Егорова on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
